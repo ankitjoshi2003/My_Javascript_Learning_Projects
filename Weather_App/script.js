67a45273e0f4eceeb94c21861b2203e251477c47
Ankit Joshi <ankit.joc624@gmail.com>
2023-10-23 13:10:40 +0530

    inital commit

--- Weather_App/script.js
+++ Weather_App/script.js
@@ -1,32 +1,42 @@
 const URL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
-const APIKEY = "";
+const APIKEY = "0374abf53f77dcd1ab7c5ba4d734b928";
 const searchBox=document.querySelector(".search input");
 const searchBtn=document.querySelector(".search button");
 const WeatherIcon=document.querySelector(".weather-icon");
 async function checkWeather(city) {
     const response = await fetch(URL + city +`&APPID=${APIKEY}`);
     var data = await response.json();
-    console.log(data);
-    document.querySelector(".city").innerHTML = data.name;
-    document.querySelector(".temp").innerHTML = data.main.temp + "°c";
-    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
-    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
-    if(data.weather[0].main=="Clouds")
+    if(response.status==404)
     {
-        WeatherIcon.src="images/clouds.png";
+        document.querySelector(".error").style.display="block";
+        document.querySelector(".weather").style.display="none";
     }
-    else if(data.weather[0].main=="Rain"){
-        WeatherIcon.src="images/rain.png";
+    else
+    {
+        document.querySelector(".error").style.display="none";
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML = data.main.temp + "°c";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+        if(data.weather[0].main=="Clouds")
+        {
+            WeatherIcon.src="images/clouds.png";
+        }
+        else if(data.weather[0].main=="Rain"){
+            WeatherIcon.src="images/rain.png";
+        }
+        else if(data.weather[0].main=="Drizzle"){
+            WeatherIcon.src="images/drizzle.png";
+        }
+        else if(data.weather[0].main=="Clear"){
+            WeatherIcon.src="images/clear.png";
+        }
+        else if(data.weather[0].main=="Snow"){
+            WeatherIcon.src="images/snow.png";
+        }
+        document.querySelector(".weather").style.display="block";
     }
-    else if(data.weather[0].main=="Drizzle"){
-        WeatherIcon.src="images/drizzle.png";
-    }
-    else if(data.weather[0].main=="Clear"){
-        WeatherIcon.src="images/clear.png";
-    }
-    else if(data.weather[0].main=="Snow"){
-        WeatherIcon.src="images/snow.png";
-    }
+   
 }
 searchBtn.addEventListener("click",()=>{
     checkWeather(searchBox.value);
